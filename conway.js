3a15eeaa8bd3c2cf0bd4b83195d888f4e9fb4907
CJ Johnson <Fermentedbeats@gmail.com>
2015-05-28 19:34:16 -0700

    converted to tables: no more div shenanigans

--- conway.js
+++ conway.js
@@ -7,30 +7,47 @@ Any dead cell with exactly three live neighbours becomes a live cell, as if by r
 
 // 1. Create 100 item array w/ 100 sub-arrays
 // 2. Link each cell to a board cell
-function createBoard() {
+//function createBoardArray() {
 
 	var boardArray = new Array(10)
 	for (var i = 0; i < boardArray.length; i++) {
 		boardArray[i] = new Array(10);
 	}
 
-	function createBoardCell(counter) {
-		var boardCell = document.createElement('div');
-		boardCell.className = "boardCell";
-		boardCell.setAttribute("className", counter);
-		return boardCell;
-	}
+	var table = document.getElementById("main");
+
+
+
+
+	// function createTableRow(counter) {
+
+	// 	var tableRow = document.createElement('tr');
+	// 	tableRow.className = "tableRow";
+	// 	tableRow.setAttribute("tableRow", counter);
+	// 	return tableRow;
+	// }
+
+	// function createTableCell(counter) {
+	// 	var tableCell = document.createElement('td');
+	// 	tableCell.className = "tableCell";
+	// 	tableCell.setAttribute("className", counter);
+	// 	return tableCell;
+	// }
 
 	for (var j = 0; j < boardArray.length; j++) {
-		
-		for (var k = 0; k < boardArray[j].length; k++) {
-			
-			document.getElementsByClassName("main")[0].appendChild(createBoardCell(k));	
+		 var row = table.insertRow(-1);
+
+		 for (var k = 0; k < 10; k++) {
+		   var cell = row.insertCell(-1);
 		}
 	}
-}
+			
+	// for (var k = 0; k < boardArray[j].length; k++) {
 
-createBoard();
+	// 	}
+//}
+
+//createBoard();
 // 3. Randomize each cell to fill with white or black (lean to 2/3 black) 
 // 4. Start: Code in the rules to run 
 // 5. Stop button
